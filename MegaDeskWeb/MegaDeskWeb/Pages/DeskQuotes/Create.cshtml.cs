aef859f06d13dcd759d808de097b30cc28b2bc58
Arthur Figueiredo <fig16009@byui.edu>
2019-11-08 13:13:16 -0700

    Kind of closing to being done

--- MegaDeskWeb/MegaDeskWeb/Pages/DeskQuotes/Create.cshtml.cs
+++ MegaDeskWeb/MegaDeskWeb/Pages/DeskQuotes/Create.cshtml.cs
@@ -13,6 +13,7 @@ namespace MegaDeskWeb.Pages.DeskQuotes
     public class CreateModel : PageModel
     {
         const decimal BASE_PRICE = 200M;
+        const decimal PRICE_PER_DRAWER = 50M;
         private readonly MegaDeskWeb.Models.MegaDeskWebContext _context;
 
         [BindProperty]
@@ -43,13 +44,13 @@ namespace MegaDeskWeb.Pages.DeskQuotes
 
         public IActionResult OnGet()
         {
-        ViewData["DeskId"] = new SelectList(_context.Set<Desk>(), "DeskId", "DeskId");
-        ViewData["ShippingId"] = new SelectList(_context.Set<Shipping>(), "ShippingId", "ShippingId");
+            ViewData["DeskId"] = new SelectList(_context.Set<Desk>(), "DeskId", "DeskId");
+            ViewData["ShippingId"] = new SelectList(_context.Set<Shipping>(), "ShippingId", "ShippingId");
             return Page();
         }
 
-        
-    public async Task<IActionResult> OnPostAsync()
+
+        public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid)
             {
@@ -61,6 +62,7 @@ namespace MegaDeskWeb.Pages.DeskQuotes
             //decimal ShippingType = DeskQuote.Shipping.shippingPrice();
 
             string ShippingType = DeskQuote.Shipping.ShippingType;
+            
             switch (ShippingType)
             {
                 case "3Day":
@@ -73,30 +75,31 @@ namespace MegaDeskWeb.Pages.DeskQuotes
                     deliveryCost = 7;
                     break;
                 default:
-                    deliveryCost = 14;
+                    deliveryCost = 0;
                     break;
             }
 
-            decimal calculateShippingPrice(decimal pvSurfaceArea, decimal pvShippingType) {
+            decimal calculateShippingPrice(decimal pvSurfaceArea, decimal pvShippingType)
+            {
 
                 decimal ShippingCost = 0M;
 
-                if(pvShippingType == 3)
+                if (pvShippingType == 3)
                 {
-                    if(pvSurfaceArea < 1000)
+                    if (pvSurfaceArea < 1000)
                     {
                         ShippingCost = 60;
                     }
-                    else if(pvSurfaceArea > 1000 && pvSurfaceArea < 2000)
+                    else if (pvSurfaceArea > 1000 && pvSurfaceArea < 2000)
                     {
                         ShippingCost = 70;
                     }
-                    else if(pvSurfaceArea > 2000)
+                    else if (pvSurfaceArea > 2000)
                     {
                         ShippingCost = 80;
                     }
                 }
-                else if(pvShippingType == 5)
+                else if (pvShippingType == 5)
                 {
                     if (pvSurfaceArea < 1000)
                     {
@@ -111,7 +114,7 @@ namespace MegaDeskWeb.Pages.DeskQuotes
                         ShippingCost = 60;
                     }
                 }
-                else if(pvShippingType == 7)
+                else if (pvShippingType == 7)
                 {
                     if (pvSurfaceArea < 1000)
                     {
@@ -133,6 +136,55 @@ namespace MegaDeskWeb.Pages.DeskQuotes
                 return ShippingCost;
             }
 
+            decimal ShippingPrice = calculateShippingPrice(SurfaceArea, deliveryCost);
+
+            decimal calculateSurfaceAreaPrice(decimal pvSurfaceArea)
+            {
+                decimal SurfaceAreaAddedCost = 0M;
+                if (pvSurfaceArea > 1000)
+                {
+                    SurfaceAreaAddedCost = pvSurfaceArea - 1000;
+                }
+                return SurfaceAreaAddedCost;
+            }
+
+            decimal SurfaceAreaPrice = calculateSurfaceAreaPrice(SurfaceArea);
+
+
+            decimal SurfaceMaterial = DeskQuote.Desk.SurfaceMaterial.SurfaceMaterialId;
+            decimal SurfaceMaterialCost = 50;
+            switch (SurfaceMaterial)
+            {
+                case 1:
+                    SurfaceMaterialCost = 200;
+                    break;
+                case 2:
+                    SurfaceMaterialCost = 100;
+                    break;
+                case 3:
+                    SurfaceMaterialCost = 50;
+                    break;
+                case 4:
+                    SurfaceMaterialCost = 300;
+                    break;
+                case 5:
+                    SurfaceMaterialCost = 125;
+                    break;
+                default:
+                    break;
+            }
+
+            decimal calculatePrice(decimal pvShippingPrice, decimal pvSurfaceMaterialPrice, decimal pvSurfaceAreaPrice, int pvNumberOfDrawers)
+            {
+                decimal finalPrice = 200M;
+
+                finalPrice += pvShippingPrice + pvSurfaceMaterialPrice + pvSurfaceAreaPrice + (pvNumberOfDrawers * PRICE_PER_DRAWER);
+
+
+                return finalPrice;
+            }
+
+            DeskQuote.QuotePrice = calculatePrice(ShippingPrice, SurfaceMaterialCost, SurfaceAreaPrice, DeskQuote.Desk.NumberOfDrawers);
             _context.DeskQuote.Add(DeskQuote);
             await _context.SaveChangesAsync();
 
