e070ee281dcacfc22f78bffa35abb4489379e20b
Arthur Figueiredo <fig16009@byui.edu>
2019-11-15 14:24:31 -0700

    Almost done. Missing Displaying surface material name and fixing edit order.

--- MegaDeskWeb/MegaDeskWeb/Models/SurfaceMaterial.cs
+++ MegaDeskWeb/MegaDeskWeb/Models/SurfaceMaterial.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace MegaDeskWeb.Models
     {
         public int SurfaceMaterialId { get; set; }
 
+        [Display(Name = "Surface Material")]
         public string SurfaceMaterialName { get; set; }
 
         public decimal Cost { get; set; }
